0f3605fefc32dcd3a3d690cfe1ad2f2e14ea6b1c
YujinKim <yujin.kim@hotmail.fr>
2017-03-13 01:22:38 +0100

    commit correction parsing_fasta

--- script/parsing_fasta-VF.py
+++ script/parsing_fasta-VF.py
@@ -12,9 +12,9 @@ for seq_record in SeqIO.parse("../data/All50Dickeya.fasta", "fasta"):
 	pos = seq_record.id.find("$")
 	fonction = []
 
-	souche = seq_record.id[0:pos]	#récupération de la souche
+	souche = seq_record.id[0:pos]
 
-	prot = seq_record.id[pos+1:]	#récupération de la protéine
+	prot = seq_record.id[pos+1:]
 
 	seq = str(seq_record.seq)
 	
@@ -26,11 +26,11 @@ for seq_record in SeqIO.parse("../data/All50Dickeya.fasta", "fasta"):
 		j = max(fonction)
 	else :
 		j=len(seq_record.description)
-	if i == 13 :	#si pas de MULTISPECIES: alors on fait rien
+	if i == 13 : #si pas de MULTISPECIES: alors on fait rien
 		i = seq_record.description.find(" ")+1
 	fct = seq_record.description[i:j]
-			
-	
+
+ 
 # Remplissage de la table Souche
 
 	cursor.execute("""SELECT * FROM SOUCHE WHERE NomS = %s AND id = %s""", (souche,idProjet))
@@ -47,29 +47,28 @@ for seq_record in SeqIO.parse("../data/All50Dickeya.fasta", "fasta"):
 
 # Remplissage de la table Contient
 
-#	cursor.execute("""SELECT idS FROM SOUCHE WHERE NomS = %s AND id = %s""",(souche,idProjet))
-#	rows = cursor.fetchall()
-#	idS = rows[0][0]
+	cursor.execute("""SELECT idS FROM SOUCHE WHERE NomS = %s AND id = %s""",(souche,idProjet))
+	rows = cursor.fetchall()
+	idS = rows[0][0]
 	cursor.execute("""SELECT idP FROM PROTEINE WHERE NomP = %s""",(prot,))
 	rows = cursor.fetchall()
 	idP = rows[0][0]
-#	cursor.execute("""INSERT INTO CONTIENT(idS,idP) VALUES(%s,%s)""", (idS,idP))
+	cursor.execute("""INSERT INTO CONTIENT(idS,idP) VALUES(%s,%s)""", (idS,idP))
 
 # Remplissage de la table Fonction
 
-	cursor.execute("""SELECT * FROM FONCTION WHERE NomFo = %s""", (fct,))
-	rows = cursor.fetchall()
-	if rows == []:
-		cursor.execute("""INSERT INTO FONCTION(NomFo) VALUES(%s)""", (fct,))
-
-	cursor.execute("""SELECT idFo FROM FONCTION WHERE NomFo = %s""", (fct,))
-	rows = cursor.fetchall()
-	idFo = rows[0][0]
-	cursor.execute("""UPDATE PROTEINE SET idFo = %s WHERE idP = %s""", (idFo, idP))
+	if len(seq_record.description.split(" ")) > 1:
+		cursor.execute("""SELECT * FROM FONCTION WHERE NomFo = %s""", (fct,))
+		rows = cursor.fetchall()
+		if rows == []:
+			cursor.execute("""INSERT INTO FONCTION(NomFo) VALUES(%s)""", (fct,))
+	
+		cursor.execute("""SELECT idFo FROM FONCTION WHERE NomFo = %s""", (fct,))
+		rows = cursor.fetchall()
+		idFo = rows[0][0]
+		cursor.execute("""UPDATE PROTEINE SET idFo = %s WHERE idP = %s""", (idFo, idP))
 
 conn.commit()
 
 conn.close()
-
-
 
